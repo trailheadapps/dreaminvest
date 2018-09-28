f307f6ec2883701d46c4ecb08353db525e0f14c4
Christophe Coenraets <ccoenraets@gmail.com>
2018-09-27 17:25:22 -0700

    DF18 changes

--- force-app/main/default/staticresources/config.js
+++ force-app/main/default/staticresources/config.js
@@ -2,7 +2,7 @@ window.config = (function() {
 
 	return {
         // Replace 'demo' with your own API key. Get one here: https://www.alphavantage.co 
-        stockAPI: 'demo'
+        stockAPI: 'demo22222'
 	};
 
 }());
